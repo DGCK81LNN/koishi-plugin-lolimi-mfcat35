1a7a57ed35bee5626ba394d6fa9c62e41f4efde6
DGCK81LNN <54282183+DGCK81LNN@users.noreply.github.com>
2024-02-08 23:26:27 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# @dgck81lnn/koishi-plugin-lolimi-mfcat35
+
+[![npm](https://img.shields.io/npm/v/@dgck81lnn/koishi-plugin-lolimi-mfcat35?style=flat-square)](https://www.npmjs.com/package/@dgck81lnn/koishi-plugin-lolimi-mfcat35)
+
+api.lolimi.cn mfcat3.5 AI聊天改进版，参考 koishi-plugin-info-fusion-assistant
+
+## 用法
+
+    指令：mfcat35 <text...>
+    AI 对话
+    在群聊中 @ 机器人或私聊机器人（指令除外）或在消息开头加冒号都可以触发 AI 对话。
---
+++ src/index.ts
@@ -0,0 +1,99 @@
+import { Context, Schema, h } from "koishi"
+import { inspect } from "node:util"
+
+export const name = "lolimi-mfcat35"
+
+export interface Config {
+  prompt: string
+  prefix: string[]
+  apiUrl: string
+}
+
+export const Config: Schema<Config> = Schema.object({
+  prompt: Schema.string()
+    .description("提示词。“@@\\_\\_BOTNAME\\_\\_@@”将被替换为机器人的昵称。")
+    .default(
+      "You are @@__BOTNAME__@@, a chatbot based on ChatGPT, " +
+        "a large language model trained by OpenAI based on the GPT-3.5 architecture.\n" +
+        "You are chatting with the user via koishi.js, a cross-platform, " +
+        "extensible and high-performance chatbot framework, " +
+        "which means most of the time your lines should be a sentence or two, " +
+        "unless the user's request requires reasoning or long-form outputs.\n" +
+        "Never use emojis, unless explicitly asked to."
+    )
+    .role("textarea"),
+  prefix: Schema.array(String)
+    .description("触发 AI 回复的消息前缀。消息首尾空格会被自动移除，以空格开头无效。")
+    .default(["：", ":"])
+    .role("table"),
+  apiUrl: Schema.string()
+    .description("API URL。提示词和用户输入文本将分别以 URL 参数 sx 和 msg 传递。")
+    .default("https://api.lolimi.cn/API/AI/mfcat3.5.php")
+    .role("textarea"),
+})
+
+function getConfiguredNickname(ctx: Context) {
+  const nickname = ctx.root.config.nickname
+  if (Array.isArray(nickname)) return nickname[0]
+  return nickname
+}
+
+export function apply(ctx: Context, config: Config) {
+  const nickname = getConfiguredNickname(ctx)
+  const logger = ctx.logger("mfcat35")
+    ctx.i18n.define("zh", require("./locales/zh"))
+
+  const cmd = ctx.command("mfcat35 <text:text>")
+  cmd.action(async ({ session }, text) => {
+    logger.debug("input " + inspect(text))
+    const response = await ctx.http.get(config.apiUrl, {
+      params: {
+        sx: config.prompt.replaceAll(
+          "@@__BOTNAME__@@",
+          nickname || session.bot.user.name || "Koishi"
+        ),
+        msg: text,
+      },
+      responseType: "text",
+    })
+    logger.debug("response " + inspect(response))
+    return response
+  })
+
+  ctx.middleware((session, next) => {
+    let ok = session.isDirect
+    let content = h
+      .transform(session.elements, {
+        at: e => {
+          if (e.id === session.bot.selfId) ok = true
+          return `@${e.type || e.name || e.id}`
+        },
+        sharp: e => `#${e.name || e.id}`,
+        a: (e, children) => `[${children.join("")}](${e.href})`,
+        image: "[Image]",
+        img: "[Image]",
+        audio: "[Attachment]",
+        video: "[Attachment]",
+        file: "[Attachment]",
+        br: "\n",
+        p: (_, children) => `\n${children.join("")}\n`,
+        message: (_, children) => `\n${children.join("")}\n`,
+        face: "[Emoji]",
+      })
+      .toString()
+      .trim()
+
+    for (const prefix of config.prefix) {
+      if (content.startsWith(prefix)) {
+        ok = true
+        content = content.slice(prefix.length).trimStart()
+        break
+      }
+    }
+
+    if (!ok) return next()
+
+    logger.debug("temporary middleware " + inspect(content))
+    return next(next => cmd.execute({ args: [content] }, next))
+  })
+}
